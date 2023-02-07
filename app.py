ceaee2efdcfba8c20b3095a54cc4ba567eeec912
CoderTaj <tajfanus@gmail.com>
2023-02-07 17:28:33 -0500

    created my first template file

--- app.py
+++ app.py
@@ -9,7 +9,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'My home page'
+    return 'My home page' 
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 
 
@@ -17,3 +23,13 @@ def home():
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+
+
+
+
+
+
+
+
